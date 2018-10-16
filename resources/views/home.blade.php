13da4e00aa8e57a27db6d002d5292168fcb05b1b
kim.biloni <kim.biloni@he-arc.ch>
2018-10-16 17:43:06 +0200

    add app description in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,21 +10,20 @@
                 </div>
 
                 <div class="card-body">
-                    <!--
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                    -->
-
-                    <ul>
-                        <li><a href="/login">login</a></li>
-                        <li><a href="/donor">sing in as a donor</a></li>
-                        <li><a href="/seeker">sing in as a seeker</a></li>
-                    </ul>
+                    <div class="card-body-links">
+                        <ul>
+                            <li><a href="/login">login</a></li>
+                            <li><a href="/donor">sing in as a donor</a></li>
+                            <li><a href="/seeker">sing in as a seeker</a></li>
+                        </ul>
+                    </div>
+                    <div class="card-body-content">
+                        <p>Tinder for assisted reproduction !</p>
+                        <p>
+                            Become a egg or sperm seeker and find the perfect donnor for your dream baby.
+                            Like tinder, swipe right to select and left to decline. It's never been easier !
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a>{{ Html::image('img/logoDreamBaby.png', 'logo of Dream Baby', array( 'width' => 70, 'height' => 70 )) }}</a>
+                
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ Html::image('img/logoDreamBaby.png', 'logo of Dream Baby', array( 'width' => 70, 'height' => 70 )) }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
