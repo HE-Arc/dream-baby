7cc384c012f7f7d26ef7610615e5624d96ddcabc
kim.biloni <kim.biloni@he-arc.ch>
2018-10-16 17:57:04 +0200

    add deadlinks in donor and seeker home views

--- resources/views/donor/home.blade.php
+++ resources/views/donor/home.blade.php
@@ -7,16 +7,22 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Dashboard</div>
-    
                     <div class="card-body">
+                        <div class="card-body-status">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
                             </div>
                         @endif
-    
                         You are logged in as donor!
-                    </div>
+                        </div>
+                        <div class="card-body-links">
+                            <ul>
+                                <li><a href="#">Questions</a></li>
+                                <li><a href="#">Private messages</a></li>
+                                <li><a href="#">Edit profil</a></li>
+                            </ul>
+                        </div>
                 </div>
             </div>
         </div>
--- resources/views/seeker/home.blade.php
+++ resources/views/seeker/home.blade.php
@@ -9,13 +9,20 @@
                     <div class="card-header">Dashboard</div>
     
                     <div class="card-body">
+                        <div class="card-body-status">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
                             </div>
                         @endif
-    
                         You are logged in as seeker!
+                        </div>
+                        <div class="card-body-links">
+                            <ul>
+                                <li><a href="#">Search a donor</a></li>
+                                <li><a href="#">Criteria</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
