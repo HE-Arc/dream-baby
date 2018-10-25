396105f8ca1f6abc7fbef9ff9be5a7e85f8876a0
Sergiy Goloviatinski <sergiy.goloviatinski@he-arc.ch>
2018-10-25 16:29:59 +0200

    added usertype table and usertype id column in users + updated register controller

--- database/migrations/2018_10_25_141632_create_user_types_table.php
+++ database/migrations/2018_10_25_141632_create_user_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUserTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_types', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('user_type_id')->references('id')->on('user_types');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('user_type_name');
             $table->timestamps();
         });
     }
@@ -31,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_types');
     }
 }
