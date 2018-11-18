d08cea309042fe8ee2f57529286422f3a4785738
kim.biloni <kim.biloni@he-arc.ch>
2018-11-18 20:14:00 +0100

    create CriteriaController

---
+++ app/Http/Controllers/CriteriaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CriteriaController extends Controller
+{
+    public function update($user_id)
+    {
+        //
+    }
+
+    public static function getSeekerCriteria($user_id)
+    {
+        //
+    }
+}
