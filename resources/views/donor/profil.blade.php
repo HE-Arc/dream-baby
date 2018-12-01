10de63fa2629ccc5671adc10f2ac8e3a03a35f44
kim.biloni <kim.biloni@he-arc.ch>
2018-12-01 18:36:03 +0100

    new donor profil design

--- resources/views/donor/profil.blade.php
+++ resources/views/donor/profil.blade.php
@@ -11,16 +11,46 @@
         </div>
     @endif
     </div>
-    <h1>{{$user->name}}</h1>
-    <img class="w-25 m-3 img-fluid" src="{{ route('donor.image', ['filename' => $donor->photo_uri]) }}" alt="{{$user->name}} - Profile Picture"/><br/>
-    <h2><strong>Sex: </strong>{{$donor->sex == 0 ? 'Male' : 'Female'}}</h2>
-    <h3><strong>Birth Date: </strong>{{date("d F Y" ,strtotime($donor->birth_date))}}</h3>
-    <h3><strong>Age: </strong>{{ floor((time() - strtotime($donor->birth_date)) / 31556926) }}</h3>
-    <h3><strong>Eye color: </strong>{{$eyecolor}}</h3>
-    <h3><strong>Hair color: </strong>{{$haircolor}}</h3>
-    <h3><strong>Ethnicity: </strong>{{$ethnicity}}</h3>
-    <h3><strong>Family antecedents: </strong>{{$donor->family_antecedents}}</h3>
-    <h3><strong>Medical antecedents: </strong>{{$donor->medical_antecedents}}</h3>
-    <h2><strong><a class="btn btn-link" href="/donor/{{$user->id}}/questions">Public questions</a></strong></h2>
+    <div class="mx-auto">
+        <h1 class="text-center">{{$user->name}}</h1>
+    </div>
+
+    <div class="text-center">
+        <img class="w-25 m-3 img-fluid" src="{{ route('donor.image', ['filename' => $donor->photo_uri]) }}" alt="{{$user->name}} - Profile Picture"/>
+    </div>
+    <div class="jumbotron text-center">
+        <div class="row swipe-info">
+            <i class="col-md-2 fa fa-transgender"></i>
+            <p class="col-md-8 text-justify" id="sex">{{$donor->sex == 0 ? 'Male' : 'Female'}}</p>
+        </div>
+        <div class="row swipe-info">
+            <i class="col-md-2 fa fa-birthday-cake"></i>
+            <p class="col-md-8 text-justify" id="birthdate">{{date("d F Y" ,strtotime($donor->birth_date))}}</p>
+        </div>
+        <div class="row swipe-info">
+            <i class="col-md-2 fa fa-eye"></i>
+            <p class="col-md-8 text-justify" id="eyecolor">{{$eyecolor}}</p>
+        </div>
+        <div class="row swipe-info">
+            <i class="col-md-2 fa fa-scissors"></i>
+            <p class="col-md-8 text-justify" id="haircolor">{{$haircolor}}</p>
+        </div>
+        <div class="row swipe-info">
+            <i class="col-md-2 fa fa-id-card-o"></i>
+            <p class="col-md-8 text-justify" id="ethnicity">{{$ethnicity}}</p>
+        </div>
+        <div class="row swipe-info">
+            <i class="col-md-2 fa fa-group"></i>
+            <p class="col-md-8 text-justify" id="family_antecedents">{{$donor->family_antecedents}}</p>
+        </div>
+        <div class="row swipe-info">
+            <i class="col-md-2 fa fa-medkit"></i>
+            <p class="col-md-8 text-justify" id="medical_antecedents">{{$donor->medical_antecedents}}</p>
+        </div>
+        
+        <div class="row">
+            <p class="col text-center"><a class="btn btn-link" href="/donor/{{$user->id}}/questions">Questions</a><p>
+        </div>
+    </div>
 </div>
 @endsection
