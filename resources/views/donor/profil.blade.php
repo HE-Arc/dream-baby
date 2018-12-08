50ab20fdeb61d933ebedcb33d6b7203478d3c645
kim.biloni <kim.biloni@he-arc.ch>
2018-12-08 15:50:48 +0100

    profil : picture is left, content is right

--- resources/views/donor/profil.blade.php
+++ resources/views/donor/profil.blade.php
@@ -11,45 +11,46 @@
         </div>
     @endif
     </div>
-    <div class="mx-auto">
-        <h1 class="text-center">{{$user->name}}</h1>
-    </div>
-
-    <div class="text-center">
-        <img class="w-25 m-3 img-fluid" src="{{ route('donor.image', ['filename' => $donor->photo_uri]) }}" alt="{{$user->name}} - Profile Picture"/>
-    </div>
-    <div class="jumbotron text-center">
-        <div class="row swipe-info">
-            <i class="col-md-2 fa fa-transgender"></i>
-            <p class="col-md-8 text-justify" id="sex">{{$donor->sex == 0 ? 'Male' : 'Female'}}</p>
+    <div class="row">
+        <div class="col">
+            <h1>{{$user->name}}</h1>
+            <div class="text-center">
+                <img class="m-3 rounded img-fluid" src="{{ route('donor.image', ['filename' => $donor->photo_uri]) }}" alt="{{$user->name}} - Profile Picture"/>
+            </div>
         </div>
-        <div class="row swipe-info">
-            <i class="col-md-2 fa fa-birthday-cake"></i>
-            <p class="col-md-8 text-justify" id="birthdate">{{date("d F Y" ,strtotime($donor->birth_date))}}</p>
-        </div>
-        <div class="row swipe-info">
-            <i class="col-md-2 fa fa-eye"></i>
-            <p class="col-md-8 text-justify" id="eyecolor">{{$eyecolor}}</p>
-        </div>
-        <div class="row swipe-info">
-            <i class="col-md-2 fa fa-scissors"></i>
-            <p class="col-md-8 text-justify" id="haircolor">{{$haircolor}}</p>
-        </div>
-        <div class="row swipe-info">
-            <i class="col-md-2 fa fa-id-card-o"></i>
-            <p class="col-md-8 text-justify" id="ethnicity">{{$ethnicity}}</p>
-        </div>
-        <div class="row swipe-info">
-            <i class="col-md-2 fa fa-group"></i>
-            <p class="col-md-8 text-justify" id="family_antecedents">{{$donor->family_antecedents}}</p>
-        </div>
-        <div class="row swipe-info">
-            <i class="col-md-2 fa fa-medkit"></i>
-            <p class="col-md-8 text-justify" id="medical_antecedents">{{$donor->medical_antecedents}}</p>
-        </div>
-        
-        <div class="row">
-            <p class="col text-center"><a class="btn btn-link" href="{{route('questions.donor', $donor->user_id)}}">Questions</a><p>
+        <div class="jumbotron text-center">
+            <div class="row swipe-info">
+                <i class="col-md-2 fa fa-transgender"></i>
+                <p class="col-md-8 text-justify" id="sex">{{$donor->sex == 0 ? 'Male' : 'Female'}}</p>
+            </div>
+            <div class="row swipe-info">
+                <i class="col-md-2 fa fa-birthday-cake"></i>
+                <p class="col-md-8 text-justify" id="birthdate">{{date("d F Y" ,strtotime($donor->birth_date))}}</p>
+            </div>
+            <div class="row swipe-info">
+                <i class="col-md-2 fa fa-eye"></i>
+                <p class="col-md-8 text-justify" id="eyecolor">{{$eyecolor}}</p>
+            </div>
+            <div class="row swipe-info">
+                <i class="col-md-2 fa fa-scissors"></i>
+                <p class="col-md-8 text-justify" id="haircolor">{{$haircolor}}</p>
+            </div>
+            <div class="row swipe-info">
+                <i class="col-md-2 fa fa-id-card-o"></i>
+                <p class="col-md-8 text-justify" id="ethnicity">{{$ethnicity}}</p>
+            </div>
+            <div class="row swipe-info">
+                <i class="col-md-2 fa fa-group"></i>
+                <p class="col-md-8 text-justify" id="family_antecedents">{{$donor->family_antecedents}}</p>
+            </div>
+            <div class="row swipe-info">
+                <i class="col-md-2 fa fa-medkit"></i>
+                <p class="col-md-8 text-justify" id="medical_antecedents">{{$donor->medical_antecedents}}</p>
+            </div>
+            
+            <div class="row">
+                <h3 class="col text-center"><a class="btn-link" href="{{route('questions.donor', $donor->user_id)}}">Questions</a><h3>
+            </div>
         </div>
     </div>
 </div>
