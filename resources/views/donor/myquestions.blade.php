fe979b63549e71909d88bf06bc716f312781b34c
Sergiy Goloviatinski <sergiy.golov@hotmail.com>
2018-12-13 14:28:00 +0100

    fixed css for questions and myquestions

--- resources/views/donor/myquestions.blade.php
+++ resources/views/donor/myquestions.blade.php
@@ -31,21 +31,24 @@
         </div>
         <div class="row">
             @if(isset($answers[$question->id]))
-            <div class="col input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="">{{$answers[$question->id]->reply}}</span>
+            <div class="col-md-11 input-group">
+                <div class="input-group-prepend ">
+                    <span class="input-group-text repliedQuestionText" id="">{{$answers[$question->id]->reply}}</span>
                 </div>
             </div>
-            <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="button">
+            <div class="col-md-1 input-group-append">
+                <button class="ml-auto btn btn-outline-secondary" type="button">
                     <a href="{{route('questions.delete', $question->id)}}" class="btn-link">{{ __('Delete') }}</a>
                 </button>
             </div>
             @else
-            <form class="replyQuestion" method="POST" action="{{route('questions.reply', $question->id)}}" enctype="multipart/form-data">
+            <div class ="col-md-11">
+            <form class="replyQuestionForm" method="POST" action="{{route('questions.reply', $question->id)}}" enctype="multipart/form-data">
+            <input type="text" name="reply" id="reply" class="form-control" placeholder="Reply..." aria-label="Reply..." aria-describedby="basic-addon2" required>
+            </div>
             @csrf
-                <div class="col input-group">
-                    <input type="text" name="reply" id="reply" class="form-control" placeholder="Reply..." aria-label="Reply..." aria-describedby="basic-addon2" required>
+                <div class="col-md-1 input-group">
+                    
                     <div class="input-group-append">
                         <button type="submit" class="btn btn-outline-secondary">
                             {{ __('Reply') }}
